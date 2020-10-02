c333c8e4d0359181dd0a9637bbc1e9c5fe5413cd
ober-man <71032009+ober-man@users.noreply.github.com>
2020-10-02 23:04:32 +0300

    Update fork_1.c

--- fork_1.c
+++ fork_1.c
@@ -17,6 +17,11 @@
 
 int main(int argc, char* argv[])
 {
+        if(argc < 2)
+	{
+		printf("Too less arguments\n");
+		return 0;
+	}
         int pid = 0;
         int n = atoi(argv[1]);
         printf("parent: pid = %d\n", getpid());
