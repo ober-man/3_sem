4a09cd681865551965b6de482a3ab14049fd8047
ober-man <oberman.ie@gmail.com>
2020-11-14 00:24:24 +0300

    Add option -f

--- 2_lesson/cp.c
+++ 2_lesson/cp.c
@@ -4,6 +4,7 @@
 #include<sys/types.h>
 #include<sys/stat.h>
 #include<fcntl.h>
+#include<errno.h>
 
 const int maxsize = 1000;
 extern char* optarg;
@@ -18,16 +19,17 @@ int main(int argc, char*argv[])
 	}
 	char* optstr = "iv";
 	int opt;
+	int f_flag = 0;
 	while((opt = getopt(argc, argv, optstr)) != -1)
 	{
 		switch(opt)
 		{
 			case 'i':
 				if(argc - 1 < optind + 1)
-                                {
-                                        printf("Too less arguments\n");
-                                        exit(-1);
-                                }
+				{
+					printf("Too less arguments\n");
+					exit(-1);
+				}
 				if(open(argv[optind+1], O_WRONLY) > 0)
 				{
 					printf("overwrite %s?", argv[optind+1]);
@@ -40,7 +42,10 @@ int main(int argc, char*argv[])
 				}
 				//else
 				break;
-
+			case 'f':
+				f_flag = 1;
+				break;
+				
 			case 'v':
 				if(argc - 1 < optind + 1)
 				{
@@ -61,10 +66,25 @@ int main(int argc, char*argv[])
 	}
 	int fd1 = open(argv[optind], O_RDONLY);
 	if(fd1 < 0)
-	{
-		printf("No such file or directory\n");
-		exit(-1);
-	}
+		if(f_flag)
+		{
+			if(unlink(argv[optind]) < 0)
+			{
+				perror("Can't delete:");
+				return errno;
+			}
+			fd1 = open(argv[optind], O_RDONLY | O_CREAT);
+			if(fd1 < 0)
+			{
+				perror("Open error:");
+				return errno;
+			}
+		}
+		else
+		{
+			printf("No such file or directory\n");
+			return -1;
+		}
 
 	int fd2 = open(argv[optind+1], O_RDWR | O_CREAT, 0777);
 
@@ -75,4 +95,5 @@ int main(int argc, char*argv[])
 
 	close(fd1);
 	close(fd2);
+	return 0;
 }
