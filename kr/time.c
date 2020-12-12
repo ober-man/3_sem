b3218b3092321065f1370b0e2e7b5c921caa5d10
ober-man <oberman.ie@gmail.com>
2020-12-12 16:27:16 +0300

    real time

--- kr/time.c
+++ kr/time.c
@@ -23,7 +23,7 @@ const int sh_size = 4096;
 const int sem_num = 2;
 #define CHILD 0
 
-struct timespec start;
+struct timespec start1, start2;
 struct timespec end;
 
 int p(int semid, int num)
@@ -32,6 +32,7 @@ int p(int semid, int num)
 	ops.sem_num = num;
 	ops.sem_op = -1;
 	int status = semop(semid, &ops, 1);
+	check(status);
 	clock_gettime(CLOCK_MONOTONIC, &end);
 	return 0;
 }
@@ -41,8 +42,8 @@ int v(int semid, int num)
 	struct sembuf ops;
 	ops.sem_num = num;
 	ops.sem_op = 1;
-	semop(semid, &ops, 1);
-	clock_gettime(CLOCK_MONOTONIC, &start);
+	int status = semop(semid, &ops, 1);
+	check(status);
 	return 0;
 }
 
@@ -61,8 +62,9 @@ int main()
                 check(pid);
                 if(pid == 0)
                 {
-			clock_gettime(CLOCK_MONOTONIC, &begin);
+			clock_gettime(CLOCK_MONOTONIC, &start1);
 			v(semid, CHILD); // CHILD == 1
+			clock_gettime(CLOCK_MONOTONIC, &start2);
 			return 0;
                 }
         }
@@ -72,8 +74,7 @@ int main()
 	for(int i = 0; i < max_size; ++i)
 	{
 		p(semid, CHILD); // wait for opportunity to get CHILD == 0
-		clock_gettime(CLOCK_MONOTONIC, &end);
-		buf[i] = end.tv_nsec - start.tv_nsec;
+		buf[i] = end.tv_nsec - (start2.tv_nsec - start1.tv_nsec);
 	}
 	
 	long sum = 0;
